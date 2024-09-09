e9ae034cdf16632bbfa0a59a2bfe546efcdd4112
bruyko-e <bruykoev@gmail.com>
2024-09-08 21:42:52 -0600

    Update helper and it's usage in the ViewController.

--- ExampleApp/Helper.swift
+++ ExampleApp/Helper.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 class Helper {
+	private var numbers: [Int] = []
+
+	func addNumber(_ number: Int) {
+		numbers.append(number)
+	}
 	
+	func getNumbers() -> [Int] {
+		numbers
+	}
 }
