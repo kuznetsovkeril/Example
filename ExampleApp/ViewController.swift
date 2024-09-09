cb2386ed74ff0f29c0da350f4cd2c979c4115503
bruyko-e <bruykoev@gmail.com>
2024-09-08 18:32:51 -0600

    Удалила лишние отступы

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by brubru on 08.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
